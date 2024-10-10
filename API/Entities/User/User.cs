d88476b9954a930318bd3cdac6098714e15365c5
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-10 15:31:16 +0200

    Added User

---
+++ API/Entities/User/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AlpimiAPI.User
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string CustomURL { get; set; }
+    }
+}
---
+++ API/Entities/User/UserController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AlpimiAPI.User.Commands;
+using AlpimiAPI.User.DTO;
+using AlpimiAPI.User.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlpimiAPI.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreateUserRequest request)
+        {
+            var command = new CreateUserCommand(
+                Guid.NewGuid(),
+                request.Login,
+                request.Password,
+                request.CustomURL
+            );
+            var res = await _mediator.Send(command);
+
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetOne([FromRoute] Guid id)
+        {
+            var query = new GetUserQuery(id);
+            User res = await _mediator.Send(query);
+
+            if (res is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(res);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        public async Task<ActionResult> Delete([FromRoute] Guid id)
+        {
+            var command = new DeleteUserCommand(id);
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}

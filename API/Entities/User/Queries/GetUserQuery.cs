96f29b7c8c5f8b2aa877415684fc21664fa48421
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-10 15:31:43 +0200

    Added get User

---
+++ API/Entities/User/Queries/GetUserQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using alpimi_planner_backend.API.Utilities;
+using Dapper;
+using MediatR;
+using Microsoft.Data.SqlClient;
+
+namespace AlpimiAPI.User.Queries
+{
+    public record GetUserQuery(Guid Id) : IRequest<User>;
+
+    public class GetBreedHandler : IRequestHandler<GetUserQuery, User>
+    {
+        public async Task<User> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            using (
+                IDbConnection connection = new SqlConnection(Configuration.GetConnectionString())
+            )
+            {
+                var user = await connection.QueryFirstOrDefaultAsync<User>(
+                    "SELECT [Id], [Login], [Password], [CustomURL] FROM [User] WHERE [Id] = @Id;",
+                    request
+                );
+
+                return user;
+            }
+        }
+    }
+}

abf0b2fd93ce95e011d02b272718662c78a9866b
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-10 15:31:32 +0200

    Added delete User

---
+++ API/Entities/User/Commands/DeleteUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using alpimi_planner_backend.API.Utilities;
+using Dapper;
+using MediatR;
+using Microsoft.Data.SqlClient;
+
+namespace AlpimiAPI.User.Commands
+{
+    public record DeleteUserCommand(Guid Id) : IRequest;
+
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand>
+    {
+        public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            using (
+                IDbConnection connection = new SqlConnection(Configuration.GetConnectionString())
+            )
+            {
+                await connection.ExecuteAsync("DELETE [User] WHERE [Id] = @Id;", request);
+            }
+        }
+    }
+}

b05cd1c06c012a0180de9210643226c15ceaad21
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-17 22:58:16 +0200

    Added unit tests to handle wrong passwords

--- Unit/Entities/User/Commands/CreateUserCommand.unit.cs
+++ Unit/Entities/User/Commands/CreateUserCommand.unit.cs
@@ -1,5 +1,6 @@
 ﻿using AlpimiAPI.User.Commands;
 using alpimi_planner_backend.API;
+using alpimi_planner_backend.API.Configuration;
 using Moq;
 using Xunit;
 
@@ -8,6 +9,7 @@ namespace alpimi_planner_backend.Unit.Entities.User.Commands
     public class CreateUserCommandUnit
     {
         private readonly Mock<IDbService> _dbService = new();
+        private readonly AuthConfiguration authConfig = new AuthConfiguration();
 
         private AlpimiAPI.User.User GetUserDetails()
         {
@@ -35,7 +37,7 @@ namespace alpimi_planner_backend.Unit.Entities.User.Commands
                 new Guid(),
                 user.Login,
                 user.CustomURL,
-                "RandomPassword"
+                "RandomPassword!1"
             );
 
             var createUserHandler = new CreateUserHandler(_dbService.Object);
@@ -43,6 +45,184 @@ namespace alpimi_planner_backend.Unit.Entities.User.Commands
             var result = await createUserHandler.Handle(createUserCommand, new CancellationToken());
 
             Assert.Equal(user.Id, result);
+        }
+
+        [Fact]
+        public async Task DoesPasswordMinLengthWorkProperly()
+        {
+            var user = GetUserDetails();
+
+            _dbService
+                .Setup(s => s.Post<AlpimiAPI.User.User>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(user);
+
+            var createUserCommand = new CreateUserCommand(
+                user.Id,
+                new Guid(),
+                user.Login,
+                user.CustomURL,
+                "Random"
+            );
+
+            var createUserHandler = new CreateUserHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createUserHandler.Handle(createUserCommand, new CancellationToken())
+            );
+            Assert.Equal(
+                "Password cannot be shorter than "
+                    + authConfig.GetMinimumPasswordLength()
+                    + " characters",
+                result.Message
+            );
+        }
+
+        [Fact]
+        public async Task DoesPasswordMaxLengthWorkProperly()
+        {
+            var user = GetUserDetails();
+
+            _dbService
+                .Setup(s => s.Post<AlpimiAPI.User.User>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(user);
+
+            var createUserCommand = new CreateUserCommand(
+                user.Id,
+                new Guid(),
+                user.Login,
+                user.CustomURL,
+                "RandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandomRandom"
+            );
+
+            var createUserHandler = new CreateUserHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createUserHandler.Handle(createUserCommand, new CancellationToken())
+            );
+            Assert.Equal(
+                "Password cannot be longer than "
+                    + authConfig.GetMaximumPasswordLength()
+                    + " characters",
+                result.Message
+            );
+        }
+
+        [Fact]
+        public async Task DoesPasswordContainsSmallLetterWorkProperly()
+        {
+            var user = GetUserDetails();
+
+            _dbService
+                .Setup(s => s.Post<AlpimiAPI.User.User>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(user);
+
+            var createUserCommand = new CreateUserCommand(
+                user.Id,
+                new Guid(),
+                user.Login,
+                user.CustomURL,
+                "RANDOMBIG1!"
+            );
+
+            var createUserHandler = new CreateUserHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createUserHandler.Handle(createUserCommand, new CancellationToken())
+            );
+            Assert.Equal(
+                "Password must contain at least one of the following: lower-case letters, upper-case letters, digits, special characters",
+                result.Message
+            );
+        }
+
+        [Fact]
+        public async Task DoesPasswordContainsBigLetterWorkProperly()
+        {
+            var user = GetUserDetails();
+
+            _dbService
+                .Setup(s => s.Post<AlpimiAPI.User.User>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(user);
+
+            var createUserCommand = new CreateUserCommand(
+                user.Id,
+                new Guid(),
+                user.Login,
+                user.CustomURL,
+                "randomsmall1!"
+            );
+
+            var createUserHandler = new CreateUserHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createUserHandler.Handle(createUserCommand, new CancellationToken())
+            );
+            Assert.Equal(
+                "Password must contain at least one of the following: lower-case letters, upper-case letters, digits, special characters",
+                result.Message
+            );
+        }
+
+        [Fact]
+        public async Task DoesPasswordContainsSymbolWorkProperly()
+        {
+            var user = GetUserDetails();
+
+            _dbService
+                .Setup(s => s.Post<AlpimiAPI.User.User>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(user);
+
+            var createUserCommand = new CreateUserCommand(
+                user.Id,
+                new Guid(),
+                user.Login,
+                user.CustomURL,
+                "Randomsmall1"
+            );
+
+            var createUserHandler = new CreateUserHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createUserHandler.Handle(createUserCommand, new CancellationToken())
+            );
+            Assert.Equal(
+                "Password must contain at least one of the following: lower-case letters, upper-case letters, digits, special characters",
+                result.Message
+            );
+        }
+
+        [Fact]
+        public async Task DoesPasswordContainsDigitWorkProperly()
+        {
+            var user = GetUserDetails();
+
+            _dbService
+                .Setup(s => s.Post<AlpimiAPI.User.User>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(user);
+
+            var createUserCommand = new CreateUserCommand(
+                user.Id,
+                new Guid(),
+                user.Login,
+                user.CustomURL,
+                "Randomsmall!"
+            );
+
+            var createUserHandler = new CreateUserHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createUserHandler.Handle(createUserCommand, new CancellationToken())
+            );
+            Assert.Equal(
+                "Password must contain at least one of the following: lower-case letters, upper-case letters, digits, special characters",
+                result.Message
+            );
         }
     }
 }

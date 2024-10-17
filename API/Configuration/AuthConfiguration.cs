2541f6d12b48253b6c10ced820728c72b17cffc9
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-17 21:39:26 +0200

    Work in progress

---
+++ API/Configuration/AuthConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using alpimi_planner_backend.API.Configuration;
+
+namespace alpimi_planner_backend.API.Settings
+{
+    public class AuthConfiguration
+    {
+        public int MinimumPasswordLength = 8;
+        public int MaximumPasswordLength = 256;
+        public RequiredCharacterTypes[]? RequiredCharacters =
+        [
+            RequiredCharacterTypes.SmallLetter,
+            RequiredCharacterTypes.BigLetter,
+            RequiredCharacterTypes.Digit,
+            RequiredCharacterTypes.Symbol
+        ];
+
+        public int GetMaximumPasswordLength()
+        {
+            return MinimumPasswordLength;
+        }
+
+        public int GetMinimumPasswordLength()
+        {
+            return MinimumPasswordLength;
+        }
+
+        public RequiredCharacterTypes[]? GetRequiredCharacters()
+        {
+            return RequiredCharacters;
+        }
+    }
+}
--- API/Utilities/Configuration.cs
+++ API/Utilities/Configuration.cs
@@ -1,4 +1,7 @@
-﻿namespace alpimi_planner_backend.API.Utilities
+﻿using alpimi_planner_backend.API.Settings;
+using Newtonsoft.Json;
+
+namespace alpimi_planner_backend.API.Utilities
 {
     public class Configuration
     {
@@ -12,10 +15,17 @@
         private static readonly string? _jwtExpire = Environment.GetEnvironmentVariable(
             "JWT_EXPIRE"
         );
+        private static readonly AuthConfiguration _authRequirements =
+            JsonConvert.DeserializeObject<AuthConfiguration>(await JsonFileReader.ReadAsync<Item>(@"C:\myFile.json"););
 
         public static string? GetConnectionString()
         {
             return _connectionString;
+        }
+
+        public static AuthConfiguration GetAuthReqiurements()
+        {
+            return _authRequirements;
         }
 
         public static string GetJWTKey()

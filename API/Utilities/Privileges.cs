723a3d708f85902257b1ef16d5687eb93e8c9801
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-22 21:32:08 +0200

    Extracted method CheckOwnership

---
+++ API/Utilities/Privileges.cs
@@ -0,0 +1,27 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AlpimiAPI.Utilities
+{
+    public static class Privileges
+    {
+        public static bool CheckOwnership(string authorization, Guid id)
+        {
+            if (authorization is null)
+            {
+                return false;
+            }
+
+            var token = authorization.ToString().Split(" ").Last();
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var jwtToken = jwtHandler.ReadJwtToken(token);
+            Claim userIdClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "userID")!;
+
+            if (Guid.Parse(userIdClaim.Value) != id)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}

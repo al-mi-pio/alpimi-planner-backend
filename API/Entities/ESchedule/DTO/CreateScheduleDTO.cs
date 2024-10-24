e055968e6ef43f69ae30f989773db403ea488496
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-25 00:43:38 +0200

    Added Schedule DTO's

---
+++ API/Entities/ESchedule/DTO/CreateScheduleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlpimiAPI.Entities.ESchedule.DTO
+{
+    public class CreateScheduleDTO
+    {
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required int SchoolHour { get; set; }
+    }
+}
---
+++ API/Entities/ESchedule/DTO/UpdateScheduleDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlpimiAPI.Entities.ESchedule.DTO
+{
+    public class UpdateScheduleDTO
+    {
+        public string? Name { get; set; }
+
+        public int? SchoolHour { get; set; }
+    }
+}

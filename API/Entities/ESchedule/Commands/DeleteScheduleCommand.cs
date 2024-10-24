5ef1266a1920f4ab9da20c19bc1fc17dee9a5f88
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-25 00:43:15 +0200

    Added DeleteSchedule

---
+++ API/Entities/ESchedule/Commands/DeleteScheduleCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace AlpimiAPI.Entities.ESchedule.Commands
+{
+    public record DeleteScheduleCommand(Guid Id) : IRequest;
+
+    public class DeleteScheduleHandler : IRequestHandler<DeleteScheduleCommand>
+    {
+        private readonly IDbService _dbService;
+
+        public DeleteScheduleHandler(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task Handle(DeleteScheduleCommand request, CancellationToken cancellationToken)
+        {
+            await _dbService.Delete("DELETE [Schedule] WHERE [Id] = @Id;", request);
+        }
+    }
+}

e17c81daaec0a00a919ba4075ed4adefc99886f7
piotr102k <piotrswiszcz2002@wp.pl>
2024-10-25 13:59:30 +0200

    Created CreateSchedules unit tests

---
+++ Test/Entities/ESchedule/Commands/CreateScheduleCommand.unit.cs
@@ -0,0 +1,99 @@
+﻿using AlpimiAPI;
+using AlpimiAPI.Entities.ESchedule;
+using AlpimiAPI.Entities.ESchedule.Commands;
+using AlpimiAPI.Entities.EUser;
+using AlpimiAPI.Entities.EUser.Commands;
+using Moq;
+using Xunit;
+
+namespace AlpimiTest.Entities.ESchedule.Commands
+{
+    public class CreateScheduleCommandUnit
+    {
+        private readonly Mock<IDbService> _dbService = new();
+
+        private User GetUserDetails()
+        {
+            var user = new User()
+            {
+                Id = new Guid(),
+                Login = "marek",
+                CustomURL = "44f"
+            };
+
+            return user;
+        }
+
+        private Schedule GetScheduleDetails()
+        {
+            var schedule = new Schedule()
+            {
+                Id = new Guid(),
+                Name = "Plan_Marka",
+                SchoolHour = 60,
+                UserID = new Guid(),
+                User = GetUserDetails()
+            };
+
+            return schedule;
+        }
+
+        [Fact]
+        public async Task CreatesSchedule()
+        {
+            var schedule = GetScheduleDetails();
+
+            _dbService
+                .Setup(s => s.Post<Schedule>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(schedule);
+
+            var createScheduleCommand = new CreateScheduleCommand(
+                schedule.Id,
+                schedule.UserID,
+                schedule.Name,
+                schedule.SchoolHour
+            );
+
+            var createScheduleHandler = new CreateScheduleHandler(_dbService.Object);
+
+            var result = await createScheduleHandler.Handle(
+                createScheduleCommand,
+                new CancellationToken()
+            );
+
+            Assert.Equal(schedule.Id, result);
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWheNameIsTaken()
+        {
+            var schedule = GetScheduleDetails();
+
+            _dbService
+                .Setup(s => s.Post<Schedule>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(schedule);
+            _dbService
+                .Setup(s => s.Get<Schedule>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(schedule);
+
+            var createScheduleCommand = new CreateScheduleCommand(
+                schedule.Id,
+                schedule.UserID,
+                "TakenName",
+                schedule.SchoolHour
+            );
+
+            var createScheduleHandler = new CreateScheduleHandler(_dbService.Object);
+
+            var result = await Assert.ThrowsAsync<BadHttpRequestException>(
+                async () =>
+                    await createScheduleHandler.Handle(
+                        createScheduleCommand,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal("Name already taken", result.Message);
+        }
+    }
+}

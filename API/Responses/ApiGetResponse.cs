e020e4bce5b92aec9539f061c69fb93ea97f5190
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-04 21:43:26 +0100

    Created ApiGetResponse

---
+++ API/Responses/ApiGetResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace AlpimiAPI.Responses
+{
+    public class ApiGetResponse<Type>
+    {
+        public Type Content { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public ApiGetResponse(Type content, DateTime time)
+        {
+            Content = content;
+            Timestamp = time;
+        }
+    }
+}

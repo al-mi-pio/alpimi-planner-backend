0653dad690a0508183e0ca86decbb4cd2a025f90
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-05 00:03:54 +0100

    Added ApiGetAllResponse

---
+++ API/Responses/ApiGetAllResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace AlpimiAPI.Responses
+{
+    public class ApiGetAllResponse<Type>
+    {
+        public Type Content { get; set; }
+        public Pagination Pagination { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public ApiGetAllResponse(Type content, Pagination pagination, DateTime timeStamp)
+        {
+            Content = content;
+            Pagination = pagination;
+            Timestamp = timeStamp;
+        }
+    }
+}
--- API/Responses/ApiGetResponse.cs
+++ API/Responses/ApiGetResponse.cs
@@ -3,6 +3,7 @@
     public class ApiGetResponse<Type>
     {
         public Type Content { get; set; }
+
         public DateTime Timestamp { get; set; }
 
         public ApiGetResponse(Type content, DateTime time)

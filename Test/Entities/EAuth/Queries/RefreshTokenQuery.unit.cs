1464eba7eacd175248676b61b94ac102dcd8ad81
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-05 21:23:15 +0100

    Added unit test to RefreshTokenQuery

---
+++ Test/Entities/EAuth/Queries/RefreshTokenQuery.unit.cs
@@ -0,0 +1,30 @@
+﻿using AlpimiAPI.Database;
+using AlpimiAPI.Entities.EAuth;
+using AlpimiAPI.Entities.EAuth.Queries;
+using AlpimiAPI.Entities.EUser;
+using Moq;
+using Xunit;
+
+namespace AlpimiTest.Entities.EAuth.Queries
+{
+    public class RefreshTokenQueryUnit
+    {
+        private readonly Mock<IDbService> _dbService = new();
+
+        [Fact]
+        public async Task GivesTokenIfLoginAndPasswordAreCorrect()
+        {
+            _dbService
+                .Setup(s => s.Get<String>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync("Token");
+
+            var refreshTokenCommand = new RefreshTokenQuery("string", new Guid(), "string");
+
+            var refreshTokenHandler = new RefreshTokenHandler();
+
+            var result = refreshTokenHandler.Handle(refreshTokenCommand, new CancellationToken());
+
+            Assert.IsType<String>(result);
+        }
+    }
+}

dff76fc434cf4d0d8120c64350b5b01ae8668ece
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-10 21:11:05 +0100

    Removed SetupSecond and changed SetupUser/SetupSchedule to require a DTO

--- Test/TestUtilities/DbHelper.cs
+++ Test/TestUtilities/DbHelper.cs
@@ -19,10 +19,8 @@ namespace AlpimiTest.TestUtilities
             return true;
         }
 
-        public static async Task<Guid> SetupUser(HttpClient _client)
+        public static async Task<Guid> SetupUser(HttpClient _client, CreateUserDTO userRequest)
         {
-            var userRequest = MockData.GetCreateUserDTODetails();
-
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
                 "Bearer",
                 TestAuthorization.GetToken("Admin", "Bob", new Guid())
@@ -34,38 +32,12 @@ namespace AlpimiTest.TestUtilities
             return jsonId!.Content;
         }
 
-        public static async Task<Guid> SetupSchedule(HttpClient _client, Guid userId)
+        public static async Task<Guid> SetupSchedule(
+            HttpClient _client,
+            Guid userId,
+            CreateScheduleDTO scheduleRequest
+        )
         {
-            var scheduleRequest = MockData.GetCreateScheduleDTODetails();
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                "Bearer",
-                TestAuthorization.GetToken("Admin", "Bob", userId)
-            );
-
-            var scheduleId = await _client.PostAsJsonAsync("/api/Schedule", scheduleRequest);
-            var jsonScheduleId = await scheduleId.Content.ReadFromJsonAsync<ApiGetResponse<Guid>>();
-
-            return jsonScheduleId!.Content;
-        }
-
-        public static async Task<Guid> SetupSecondUser(HttpClient _client)
-        {
-            var userRequest = MockData.GetCreateSecondUserDTODetails();
-
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                "Bearer",
-                TestAuthorization.GetToken("Admin", "Bob", new Guid())
-            );
-
-            var userId = await _client.PostAsJsonAsync("/api/User", userRequest);
-            var jsonId = await userId.Content.ReadFromJsonAsync<ApiGetResponse<Guid>>();
-
-            return jsonId!.Content;
-        }
-
-        public static async Task<Guid> SetupSecondSchedule(HttpClient _client, Guid userId)
-        {
-            var scheduleRequest = MockData.GetCreateSecondScheduleDTODetails();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
                 "Bearer",
                 TestAuthorization.GetToken("Admin", "Bob", userId)

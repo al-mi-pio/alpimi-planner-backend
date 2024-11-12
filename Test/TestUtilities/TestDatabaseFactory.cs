c7ab627bcf5fb403ee1a04eee1b1f4f794c1dec4
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-12 17:34:36 +0100

    Updated TestDatabaseFactory to use .env files

--- Test/TestUtilities/TestDatabaseFactory.cs
+++ Test/TestUtilities/TestDatabaseFactory.cs
@@ -1,21 +1,7 @@
 ﻿using System.Data;
-using System.Data.Common;
-using AlpimiAPI.Database;
-using AlpimiAPI.Entities.EAuth;
-using AlpimiAPI.Entities.EUser;
 using AlpimiAPI.Utilities;
-using Dapper;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using Moq;
-using Xunit;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace AlpimiTest.TestUtilities
 {
@@ -24,22 +10,16 @@ namespace AlpimiTest.TestUtilities
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseContentRoot(GetProjectContentRoot());
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
             builder.UseEnvironment("Testing");
             builder.ConfigureServices(
                 (context, services) =>
                 {
                     services.AddScoped<IDbConnection>(sp => new SqlConnection(
-                        "Data Source=DESKTOP-F50MD76;Initial Catalog=testy2;Integrated Security=True;Pooling=False;Encrypt=True;Trust Server Certificate=True"
+                        Configuration.GetTestConnectionString()
                     ));
                 }
             );
-        }
-
-        private string GetProjectContentRoot()
-        {
-            var projectDir = Directory.GetCurrentDirectory();
-            return Path.Combine(projectDir, "C:/Users/Piotr/source/repos/alpimi-planner-backend");
         }
     }
 }

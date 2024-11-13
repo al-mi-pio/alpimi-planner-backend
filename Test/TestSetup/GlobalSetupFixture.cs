6053bc5fd4bae2144afc6a19a163aa36861b4d5c
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-13 13:32:10 +0100

    Removed unnecessary env load, and replaced it with initialization

--- Test/TestSetup/GlobalSetupFixture.cs
+++ Test/TestSetup/GlobalSetupFixture.cs
@@ -3,22 +3,6 @@ using Xunit;
 
 namespace AlpimiTest.TestSetup
 {
-    using DotNetEnv;
-    using Xunit;
-
-    public class EnvSetupFixture
-    {
-        static EnvSetupFixture()
-        {
-            DotNetEnv.Env.Load(
-                Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", ".env")
-            );
-        }
-    }
-
     [CollectionDefinition("Sequential Tests")]
-    public class SequentialTestCollection : ICollectionFixture<EnvSetupFixture> { }
-
-    [CollectionDefinition("Global setup collection")]
-    public class GlobalSetupCollection : ICollectionFixture<EnvSetupFixture> { }
+    public class SequentialTestCollection { }
 }

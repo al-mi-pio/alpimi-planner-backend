13037e26d9a048706ece9af4e4f75e15d4aaefbe
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-15 16:33:09 +0100

    Added rate limit settings

---
+++ API/Settings/RateLimiterSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AlpimiAPI.Settings
+{
+    public static class RateLimiterSettings
+    {
+        public const int permitLimit = 20;
+        public static TimeSpan timeWindow = TimeSpan.FromSeconds(5);
+    }
+}

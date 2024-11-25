0b304f030dba3bd5383bc4837c7706f11ccee3e7
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-25 22:01:15 +0100

    Added Teacher entity

---
+++ API/Entities/ETeacher/DTO/UpdateTeacherDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AlpimiAPI.Entities.ETeacher.DTO
+{
+    public class UpdateTeacherDTO
+    {
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+    }
+}

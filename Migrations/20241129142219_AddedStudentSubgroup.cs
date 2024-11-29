08dae9245612d9e610a3f24cfba09df614598aef
piotr102k <piotrswiszcz2002@wp.pl>
2024-11-29 17:22:17 +0100

    Addec new entity to database

---
+++ Migrations/20241129142219_AddedStudentSubgroup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alpimi_planner_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStudentSubgroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentSubgroup",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubgroupId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentSubgroup", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentSubgroup_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentSubgroup_StudentId",
+                table: "StudentSubgroup",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentSubgroup");
+        }
+    }
+}

d1c92f0920ebad1cc903b512d3093fdeb9870617
piotr102k <piotrswiszcz2002@wp.pl>
2024-12-02 23:00:21 +0100

    Moved StudentSubgroup to new many to many relations folder

---
+++ API/Relations/StudentSubgroup.cs
@@ -0,0 +1,12 @@
+﻿using AlpimiAPI.Entities.EStudent;
+
+namespace AlpimiAPI.Relations
+{
+    public class StudentSubgroup
+    {
+        public Guid Id { get; set; }
+        public required Guid StudentId { get; set; }
+        public required Student Student { get; set; }
+        public required Guid SubgroupId { get; set; }
+    }
+}

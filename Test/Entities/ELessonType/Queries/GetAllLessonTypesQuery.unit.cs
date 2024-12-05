11c327a2f0355962c8a799771ea533fd372a15dc
piotr102k <piotrswiszcz2002@wp.pl>
2024-12-05 22:30:56 +0100

    Fixed missing get all tests

--- Test/Entities/ELessonType/Queries/GetAllLessonTypesQuery.unit.cs
+++ Test/Entities/ELessonType/Queries/GetAllLessonTypesQuery.unit.cs
@@ -1,4 +1,178 @@
-﻿namespace alpimi_planner_backend.Test.Entities.ELessonType.Queries
+﻿using AlpimiAPI.Database;
+using AlpimiAPI.Entities.ELessonType.Queries;
+using AlpimiAPI.Locales;
+using AlpimiAPI.Responses;
+using AlpimiTest.TestSetup;
+using Microsoft.Extensions.Localization;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AlpimiTest.Entities.ELessonType.Queries
 {
-    public class GetAllLessonTypes { }
+    [Collection("Sequential Tests")]
+    public class GetAllLessonTypesQueryUnit
+    {
+        private readonly Mock<IDbService> _dbService = new();
+        private readonly Mock<IStringLocalizer<Errors>> _str;
+
+        public GetAllLessonTypesQueryUnit()
+        {
+            _str = ResourceSetup.Setup();
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectPerPageIsGiven()
+        {
+            var getAllLessonTypeQuery = new GetAllLessonTypesQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(-20, 0, "Id", "ASC")
+            );
+            var getAllLessonTypeHandler = new GetAllLessonTypesHandler(
+                _dbService.Object,
+                _str.Object
+            );
+
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllLessonTypeHandler.Handle(
+                        getAllLessonTypeQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("PerPage parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectPageIsGiven()
+        {
+            var getAllLessonTypeQuery = new GetAllLessonTypesQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, -1, "Id", "ASC")
+            );
+            var getAllLessonTypeHandler = new GetAllLessonTypesHandler(
+                _dbService.Object,
+                _str.Object
+            );
+
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllLessonTypeHandler.Handle(
+                        getAllLessonTypeQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("Page parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectSortByIsGiven()
+        {
+            var getAllLessonTypeQuery = new GetAllLessonTypesQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, 0, "wrong", "ASC")
+            );
+            var getAllLessonTypeHandler = new GetAllLessonTypesHandler(
+                _dbService.Object,
+                _str.Object
+            );
+
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllLessonTypeHandler.Handle(
+                        getAllLessonTypeQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("SortBy parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectSortOrderIsGiven()
+        {
+            var getAllLessonTypeQuery = new GetAllLessonTypesQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, 0, "Id", "wrong")
+            );
+            var getAllLessonTypeHandler = new GetAllLessonTypesHandler(
+                _dbService.Object,
+                _str.Object
+            );
+
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllLessonTypeHandler.Handle(
+                        getAllLessonTypeQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("SortOrder parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsMultipleErrorMessages()
+        {
+            var getAllLessonTypeQuery = new GetAllLessonTypesQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, 0, "wrong", "wrong")
+            );
+            var getAllLessonTypeHandler = new GetAllLessonTypesHandler(
+                _dbService.Object,
+                _str.Object
+            );
+
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllLessonTypeHandler.Handle(
+                        getAllLessonTypeQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[]
+                    {
+                        new ErrorObject("SortOrder parameter is invalid"),
+                        new ErrorObject("SortBy parameter is invalid")
+                    }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+    }
 }

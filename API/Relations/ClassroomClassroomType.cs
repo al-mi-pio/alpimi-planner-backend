7c651db5cd05e0cfc084db6ce1130ae0308fcd33
piotr102k <piotrswiszcz2002@wp.pl>
2024-12-09 23:39:21 +0100

    Added Classroom to ClassroomType relation to database

---
+++ API/Relations/ClassroomClassroomType.cs
@@ -0,0 +1,12 @@
+﻿using AlpimiAPI.Entities.EClassroom;
+
+namespace AlpimiAPI.Relations
+{
+    public class ClassroomClassroomType
+    {
+        public Guid Id { get; set; }
+        public required Guid ClassroomId { get; set; }
+        public required Classroom Classroom { get; set; }
+        public required Guid ClassroomTypeId { get; set; }
+    }
+}

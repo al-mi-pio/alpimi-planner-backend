979ef366a1ca61eed5f4e5de76d442dd6beb7bac
piotr102k <piotrswiszcz2002@wp.pl>
2025-05-24 14:17:53 +0200

    Added Availability unit tests

---
+++ Test/Entities/EAvailability/Commands/UpdateAvailability.unit.cs
@@ -0,0 +1,204 @@
+﻿using AlpimiAPI.Database;
+using AlpimiAPI.Entities.EAvailability;
+using AlpimiAPI.Entities.EAvailability.Commands;
+using AlpimiAPI.Entities.EScheduleSettings;
+using AlpimiAPI.Entities.ETeacher;
+using AlpimiAPI.Locales;
+using AlpimiAPI.Responses;
+using AlpimiTest.TestSetup;
+using AlpimiTest.TestUtilities;
+using Microsoft.Extensions.Localization;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AlpimiTest.Entities.EAvailability.Commands
+{
+    [Collection("Sequential Tests")]
+    public class UpdateAvailabilityCommandUnit
+    {
+        private readonly Mock<IDbService> _dbService = new();
+        private readonly Mock<IStringLocalizer<Errors>> _str;
+
+        public UpdateAvailabilityCommandUnit()
+        {
+            _str = ResourceSetup.Setup();
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenWrongTeacherIdIsGiven()
+        {
+            var dto = MockData.GetUpdateAvailabilityDTODetails();
+            dto.TeacherId = new Guid();
+            _dbService
+                .Setup(s => s.Get<Availability>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetAvailabilityDetails());
+
+            var updateAvailabilityCommand = new UpdateAvailabilityCommand(
+                new Guid(),
+                dto,
+                new Guid(),
+                "User"
+            );
+            var updateAvailabilityHandler = new UpdateAvailabilityHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await updateAvailabilityHandler.Handle(
+                        updateAvailabilityCommand,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[]
+                    {
+                        new ErrorObject(
+                            "Teacher with id 00000000-0000-0000-0000-000000000000 was not found"
+                        )
+                    }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenStartIsAfterEnd()
+        {
+            _dbService
+                .Setup(s => s.Get<Availability>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetAvailabilityDetails());
+            _dbService
+                .Setup(s => s.Get<Teacher>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetTeacherDetails());
+            _dbService
+                .Setup(s => s.Get<ScheduleSettings>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetScheduleSettingsDetails());
+            _dbService
+                .Setup(s => s.Get<int>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(5);
+
+            var updateRequest = MockData.GetUpdateAvailabilityDTODetails();
+            updateRequest.End = 3;
+            updateRequest.Start = 4;
+            var updateAvailabilityCommand = new UpdateAvailabilityCommand(
+                new Guid(),
+                updateRequest,
+                new Guid(),
+                "User"
+            );
+            var updateAvailabilityHandler = new UpdateAvailabilityHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await updateAvailabilityHandler.Handle(
+                        updateAvailabilityCommand,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[]
+                    {
+                        new ErrorObject("The end time cannot happen before the start time")
+                    }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenStartIsLessThan1()
+        {
+            _dbService
+                .Setup(s => s.Get<Availability>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetAvailabilityDetails());
+            _dbService
+                .Setup(s => s.Get<Teacher>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetTeacherDetails());
+            _dbService
+                .Setup(s => s.Get<ScheduleSettings>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetScheduleSettingsDetails());
+            _dbService
+                .Setup(s => s.Get<int>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(5);
+
+            var updateRequest = MockData.GetUpdateAvailabilityDTODetails();
+            updateRequest.Start = 0;
+            var updateAvailabilityCommand = new UpdateAvailabilityCommand(
+                new Guid(),
+                updateRequest,
+                new Guid(),
+                "User"
+            );
+            var updateAvailabilityHandler = new UpdateAvailabilityHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await updateAvailabilityHandler.Handle(
+                        updateAvailabilityCommand,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("Start parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenEndIsMoreThanTheAmountOfPeriods()
+        {
+            _dbService
+                .Setup(s => s.Get<Availability>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetAvailabilityDetails());
+            _dbService
+                .Setup(s => s.Get<Teacher>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetTeacherDetails());
+            _dbService
+                .Setup(s => s.Get<ScheduleSettings>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(MockData.GetScheduleSettingsDetails());
+            _dbService
+                .Setup(s => s.Get<int>(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(5);
+
+            var updateRequest = MockData.GetUpdateAvailabilityDTODetails();
+            updateRequest.End = 6;
+            var updateAvailabilityCommand = new UpdateAvailabilityCommand(
+                new Guid(),
+                updateRequest,
+                new Guid(),
+                "User"
+            );
+            var updateAvailabilityHandler = new UpdateAvailabilityHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await updateAvailabilityHandler.Handle(
+                        updateAvailabilityCommand,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("End parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+    }
+}
---
+++ Test/Entities/EAvailability/Queries/GetAllAvailability.unit.cs
@@ -0,0 +1,173 @@
+﻿using AlpimiAPI.Database;
+using AlpimiAPI.Entities.EAvailability.Queries;
+using AlpimiAPI.Locales;
+using AlpimiAPI.Responses;
+using AlpimiTest.TestSetup;
+using Microsoft.Extensions.Localization;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace AlpimiTest.Entities.EAvailability.Queries
+{
+    [Collection("Sequential Tests")]
+    public class GetAllAvailabilityByTeacherQueryUnit
+    {
+        private readonly Mock<IDbService> _dbService = new();
+        private readonly Mock<IStringLocalizer<Errors>> _str;
+
+        public GetAllAvailabilityByTeacherQueryUnit()
+        {
+            _str = ResourceSetup.Setup();
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectPerPageIsGiven()
+        {
+            var getAllAvailabilityQuery = new GetAllAvailabilityByTeacherQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(-20, 0, "Id", "ASC")
+            );
+            var getAllAvailabilityHandler = new GetAllAvailabilityByTeacherHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllAvailabilityHandler.Handle(
+                        getAllAvailabilityQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("PerPage parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectPageIsGiven()
+        {
+            var getAllAvailabilityQuery = new GetAllAvailabilityByTeacherQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, -1, "Id", "ASC")
+            );
+            var getAllAvailabilityHandler = new GetAllAvailabilityByTeacherHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllAvailabilityHandler.Handle(
+                        getAllAvailabilityQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("Page parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectSortByIsGiven()
+        {
+            var getAllAvailabilityQuery = new GetAllAvailabilityByTeacherQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, 0, "wrong", "ASC")
+            );
+            var getAllAvailabilityHandler = new GetAllAvailabilityByTeacherHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllAvailabilityHandler.Handle(
+                        getAllAvailabilityQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("SortBy parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenIncorrectSortOrderIsGiven()
+        {
+            var getAllAvailabilityQuery = new GetAllAvailabilityByTeacherQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, 0, "Id", "wrong")
+            );
+            var getAllAvailabilityHandler = new GetAllAvailabilityByTeacherHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllAvailabilityHandler.Handle(
+                        getAllAvailabilityQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[] { new ErrorObject("SortOrder parameter is invalid") }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+
+        [Fact]
+        public async Task ThrowsMultipleErrorMessages()
+        {
+            var getAllAvailabilityQuery = new GetAllAvailabilityByTeacherQuery(
+                new Guid(),
+                new Guid(),
+                "Admin",
+                new PaginationParams(20, 0, "wrong", "wrong")
+            );
+            var getAllAvailabilityHandler = new GetAllAvailabilityByTeacherHandler(
+                _dbService.Object,
+                _str.Object
+            );
+            var result = await Assert.ThrowsAsync<ApiErrorException>(
+                async () =>
+                    await getAllAvailabilityHandler.Handle(
+                        getAllAvailabilityQuery,
+                        new CancellationToken()
+                    )
+            );
+
+            Assert.Equal(
+                JsonConvert.SerializeObject(
+                    new ErrorObject[]
+                    {
+                        new ErrorObject("SortOrder parameter is invalid"),
+                        new ErrorObject("SortBy parameter is invalid")
+                    }
+                ),
+                JsonConvert.SerializeObject(result.errors)
+            );
+        }
+    }
+}

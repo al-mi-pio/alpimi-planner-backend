5866fe5edb56b07f368f83adc7f4002e8732925e
piotr102k <piotrswiszcz2002@wp.pl>
2025-05-27 17:28:15 +0200

    Created Collision endpoint

---
+++ API/Entities/ECollision/CollisionController.cs
@@ -0,0 +1,81 @@
+﻿using AlpimiAPI.Entities.ECollision.DTO;
+using AlpimiAPI.Entities.ECollision.Queries;
+using AlpimiAPI.Locales;
+using AlpimiAPI.Responses;
+using AlpimiAPI.Utilities;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Localization;
+
+namespace AlpimiAPI.Entities.ECollision
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(ApiErrorResponse), 429)]
+    [EnableRateLimiting("FixedWindow")]
+    public class CollisionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IStringLocalizer<Errors> _str;
+
+        public CollisionController(IMediator mediator, IStringLocalizer<Errors> str)
+        {
+            _mediator = mediator;
+            _str = str;
+        }
+
+        /// <summary>
+        /// Gets all Collisions by ScheduleId or CollisionTypeId
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 400)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 401)]
+        public async Task<ActionResult<ApiGetAllResponse<IEnumerable<CollisionDTO>>>> GetAll(
+            [FromHeader] string? Authorization,
+            [FromQuery] Guid id,
+            [FromQuery] int perPage = Configuration.perPage,
+            [FromQuery] int page = Configuration.page,
+            [FromQuery] string sortBy = Configuration.sortBy,
+            [FromQuery] string sortOrder = Configuration.sortOrder
+        )
+        {
+            Guid filteredId = Privileges.GetUserIdFromToken(Authorization);
+            string privileges = Privileges.GetUserRoleFromToken(Authorization);
+
+            var query = new GetAllCollisionsQuery(
+                id,
+                filteredId,
+                privileges,
+                new PaginationParams(perPage, (page - 1) * perPage, sortBy, sortOrder)
+            );
+            try
+            {
+                (IEnumerable<Collision>?, int) result = await _mediator.Send(query);
+
+                var response = new ApiGetAllResponse<IEnumerable<CollisionDTO>>(
+                    result.Item1!.Select(DataTrimmer.Trim),
+                    new Pagination(result.Item2, perPage, page, sortBy, sortOrder)
+                );
+                return Ok(response);
+            }
+            catch (ApiErrorException ex)
+            {
+                return BadRequest(new ApiErrorResponse(400, ex.errors));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(
+                    new ApiErrorResponse(400, [new ErrorObject(_str["unknownError", ex])])
+                );
+            }
+        }
+    }
+}
---
+++ API/Entities/ECollision/Queries/GetAllCollisionsQuery.cs
@@ -0,0 +1,160 @@
+﻿using AlpimiAPI.Database;
+using AlpimiAPI.Entities.ECollisionType;
+using AlpimiAPI.Entities.ECollisionType.Queries;
+using AlpimiAPI.Locales;
+using AlpimiAPI.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace AlpimiAPI.Entities.ECollision.Queries
+{
+    public record GetAllCollisionsQuery(
+        Guid Id,
+        Guid FilteredId,
+        string Role,
+        PaginationParams Pagination
+    ) : IRequest<(IEnumerable<Collision>?, int)>;
+
+    public class GetAllCollisionsHandler
+        : IRequestHandler<GetAllCollisionsQuery, (IEnumerable<Collision>?, int)>
+    {
+        private readonly IDbService _dbService;
+        private readonly IStringLocalizer<Errors> _str;
+
+        public GetAllCollisionsHandler(IDbService dbService, IStringLocalizer<Errors> str)
+        {
+            _dbService = dbService;
+            _str = str;
+        }
+
+        public async Task<(IEnumerable<Collision>?, int)> Handle(
+            GetAllCollisionsQuery request,
+            CancellationToken cancellationToken
+        )
+        {
+            List<ErrorObject> errors = new List<ErrorObject>();
+            if (request.Pagination.PerPage < 0)
+            {
+                errors.Add(new ErrorObject(_str["badParameter", "PerPage"]));
+            }
+            if (request.Pagination.Offset < 0)
+            {
+                errors.Add(new ErrorObject(_str["badParameter", "Page"]));
+            }
+            if (
+                request.Pagination.SortOrder.ToLower() != "asc"
+                && request.Pagination.SortOrder.ToLower() != "desc"
+            )
+            {
+                errors.Add(new ErrorObject(_str["badParameter", "SortOrder"]));
+            }
+            if (
+                request.Pagination.SortBy != "Id"
+                && request.Pagination.SortBy != "Name"
+                && request.Pagination.SortBy != "StudentCount"
+            )
+            {
+                errors.Add(new ErrorObject(_str["badParameter", "SortBy"]));
+            }
+
+            if (errors.Count != 0)
+            {
+                throw new ApiErrorException(errors);
+            }
+
+            IEnumerable<Collision>? collisions;
+            int count;
+            switch (request.Role)
+            {
+                case "Admin":
+                    count = await _dbService.Get<int>(
+                        @"
+                            SELECT 
+                            COUNT(*)
+                            FROM [Collision] c
+                            INNER JOIN [CollisionType] ct ON ct.[Id] = c.[CollisionTypeId]
+                            WHERE c.[CollisionTypeId] = @Id OR ct.[ScheduleId] = @Id;",
+                        request
+                    );
+                    collisions = await _dbService.GetAll<Collision>(
+                        $@"
+                            SELECT
+                            c.[Id], [CollidingObject1], [CollidingObject2], [Ignored], c.[CollisionTypeId]
+                            FROM [Collision] c
+                            INNER JOIN [CollisionType] ct ON ct.[Id] = c.[CollisionTypeId]
+                            WHERE c.[CollisionTypeId] = @Id OR ct.[ScheduleId] = @Id
+                            ORDER BY
+                            {request.Pagination.SortBy}
+                            {request.Pagination.SortOrder}
+                            OFFSET
+                            {request.Pagination.Offset} ROWS
+                            FETCH NEXT
+                            {request.Pagination.PerPage} ROWS ONLY;",
+                        request
+                    );
+                    break;
+                default:
+                    count = await _dbService.Get<int>(
+                        @"
+                            SELECT
+                            COUNT(*)
+                            FROM [Collision] c
+                            INNER JOIN [CollisionType] ct ON ct.[Id] = c.[CollisionTypeId]
+                            INNER JOIN [Schedule] s ON s.[Id] = ct.[ScheduleId]
+                            INNER JOIN [ScheduleSettings] ss ON ss.[ScheduleId] = s.[Id]
+                            WHERE s.[UserId] = @FilteredId AND (c.[CollisionTypeId] = @Id OR ct.[ScheduleId] = @Id);",
+                        request
+                    );
+                    collisions = await _dbService.GetAll<Collision>(
+                        $@"
+                            SELECT 
+                            c.[Id], [CollidingObject1], [CollidingObject2], [Ignored], c.[CollisionTypeId]
+                            FROM [Collision] c
+                            INNER JOIN [CollisionType] ct ON ct.[Id] = c.[CollisionTypeId]
+                            INNER JOIN [Schedule] s ON s.[Id] = ct.[ScheduleId]
+                            INNER JOIN [ScheduleSettings] ss ON ss.[ScheduleId] = s.[Id]
+                            WHERE s.[UserId] = @FilteredId AND (c.[CollisionTypeId] = @Id OR ct.[ScheduleId] = @Id)
+                            ORDER BY
+                            {request.Pagination.SortBy}
+                            {request.Pagination.SortOrder}
+                            OFFSET
+                            {request.Pagination.Offset} ROWS
+                            FETCH NEXT
+                            {request.Pagination.PerPage} ROWS ONLY;",
+                        request
+                    );
+                    break;
+            }
+
+            if (collisions != null)
+            {
+                Dictionary<Guid, CollisionType> collisionTypeMap =
+                    new Dictionary<Guid, CollisionType>();
+                foreach (var collision in collisions)
+                {
+                    if (!collisionTypeMap.ContainsKey(collision.CollisionTypeId))
+                    {
+                        GetCollisionTypeHandler getCollisionTypeHandler =
+                            new GetCollisionTypeHandler(_dbService);
+                        GetCollisionTypeQuery getCollisionTypeQuery = new GetCollisionTypeQuery(
+                            collision.CollisionTypeId,
+                            new Guid(),
+                            "Admin"
+                        );
+                        ActionResult<CollisionType?> collisionType =
+                            await getCollisionTypeHandler.Handle(
+                                getCollisionTypeQuery,
+                                cancellationToken
+                            );
+
+                        collisionTypeMap.Add(collision.CollisionTypeId, collisionType.Value!);
+                    }
+                    collision.CollisionType = collisionTypeMap[collision.CollisionTypeId];
+                }
+            }
+
+            return (collisions, count);
+        }
+    }
+}

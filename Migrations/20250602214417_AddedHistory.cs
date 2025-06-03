1250d41928d72c5c98e11371c628cd07ad6ac123
piotr102k <piotrswiszcz2002@wp.pl>
2025-06-03 18:08:02 +0200

    Added History to DB

---
+++ Migrations/20250602214417_AddedHistory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alpimi_planner_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "History",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AffectedEntityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AffectedEntity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Command = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReversaleDTO = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsUndone = table.Column<bool>(type: "bit", nullable: false),
+                    CollisionChecked = table.Column<bool>(type: "bit", nullable: false),
+                    ScheduleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_History", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_History_Schedule_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedule",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_History_ScheduleId",
+                table: "History",
+                column: "ScheduleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "History");
+        }
+    }
+}

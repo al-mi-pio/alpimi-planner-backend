ecd4118bc5cf82920537b1abd1a4d6622c2c6e3b
piotr102k <piotrswiszcz2002@wp.pl>
2025-06-03 18:10:29 +0200

    Implemented redo and undo in Lesson

--- API/Entities/ELesson/Commands/DeleteLessonCommand.cs
+++ API/Entities/ELesson/Commands/DeleteLessonCommand.cs
@@ -1,5 +1,11 @@
-﻿using AlpimiAPI.Database;
+﻿using System.Text.Json;
+using AlpimiAPI.Database;
+using AlpimiAPI.Entities.EDayOff.Commands;
+using AlpimiAPI.Entities.EHistory.DTO;
+using AlpimiAPI.Entities.ELesson.DTO;
+using AlpimiAPI.Entities.ELesson.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlpimiAPI.Entities.ELesson.Commands
 {
@@ -16,6 +22,41 @@ namespace AlpimiAPI.Entities.ELesson.Commands
 
         public async Task Handle(DeleteLessonCommand request, CancellationToken cancellationToken)
         {
+            GetLessonHandler getLessonHandler = new GetLessonHandler(_dbService);
+            GetLessonQuery getLessonQuery = new GetLessonQuery(
+                request.Id,
+                request.FilteredId,
+                request.Role
+            );
+            ActionResult<Lesson?> lesson = await getLessonHandler.Handle(
+                getLessonQuery,
+                cancellationToken
+            );
+            if (lesson.Value != null)
+            {
+                CreateLessonDTO reversaleDTO = new CreateLessonDTO
+                {
+                    Name = lesson.Value.Name,
+                    AmountOfHours = lesson.Value.AmountOfHours,
+                    LessonTypeId = lesson.Value.LessonTypeId,
+                    TeacherId = lesson.Value.TeacherId,
+                    SubgroupIds = []
+                };
+                AddToHistoryHandler addToHistoryHandler = new AddToHistoryHandler(_dbService);
+                AddToHistoryDTO addToHistoryDTO = new AddToHistoryDTO
+                {
+                    Id = Guid.NewGuid(),
+                    Timestamp = DateTime.Now,
+                    AffectedEntityId = request.Id,
+                    AffectedEntity = "Lesson",
+                    Command = "Delete",
+                    ReversaleDTO = JsonSerializer.Serialize(reversaleDTO),
+                    CollisionChecked = true,
+                    ScheduleId = lesson.Value.LessonType.ScheduleId,
+                };
+                AddToHistoryCommand addToHistoryCommand = new AddToHistoryCommand(addToHistoryDTO);
+                await addToHistoryHandler.Handle(addToHistoryCommand, cancellationToken);
+            }
             switch (request.Role)
             {
                 case "Admin":
--- API/Entities/ELesson/Commands/UpdateLessonCommand.cs
+++ API/Entities/ELesson/Commands/UpdateLessonCommand.cs
@@ -1,6 +1,9 @@
-﻿using AlpimiAPI.Database;
+﻿using System.Text.Json;
+using AlpimiAPI.Database;
 using AlpimiAPI.Entities.EClassroomType;
 using AlpimiAPI.Entities.EClassroomType.Queries;
+using AlpimiAPI.Entities.EDayOff.Commands;
+using AlpimiAPI.Entities.EHistory.DTO;
 using AlpimiAPI.Entities.ELesson.DTO;
 using AlpimiAPI.Entities.ELesson.Queries;
 using AlpimiAPI.Entities.ELessonType;
@@ -61,6 +64,14 @@ namespace AlpimiAPI.Entities.ELesson.Commands
             {
                 return null;
             }
+
+            UpdateLessonDTO reversaleDTOLesson = new UpdateLessonDTO
+            {
+                Name = originalLesson.Value!.Name,
+                AmountOfHours = originalLesson.Value.AmountOfHours,
+                TeacherId = originalLesson.Value.TeacherId,
+                LessonTypeId = originalLesson.Value.LessonTypeId,
+            };
 
             request.dto.Name = request.dto.Name ?? originalLesson.Value!.Name;
             request.dto.AmountOfHours =
@@ -354,6 +365,21 @@ namespace AlpimiAPI.Entities.ELesson.Commands
 
             lesson!.LessonType = lessonType.Value;
             lesson.Teacher = teacher.Value;
+
+            AddToHistoryHandler addToHistoryHandler = new AddToHistoryHandler(_dbService);
+            AddToHistoryDTO addToHistoryDTO = new AddToHistoryDTO
+            {
+                Id = Guid.NewGuid(),
+                Timestamp = DateTime.Now,
+                AffectedEntityId = request.Id,
+                AffectedEntity = "Lesson",
+                Command = "Patch",
+                ReversaleDTO = JsonSerializer.Serialize(reversaleDTOLesson),
+                CollisionChecked = false,
+                ScheduleId = originalLesson.Value.LessonType.ScheduleId,
+            };
+            AddToHistoryCommand addToHistoryCommand = new AddToHistoryCommand(addToHistoryDTO);
+            await addToHistoryHandler.Handle(addToHistoryCommand, cancellationToken);
 
             return lesson;
         }

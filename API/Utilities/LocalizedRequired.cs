01e3dc00fc533e644763beb9a53ab28028526029
piotr102k <piotrswiszcz2002@wp.pl>
2025-07-20 16:37:15 +0200

    Create LocalizedRequired that uses locales

---
+++ API/Utilities/LocalizedRequired.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AlpimiAPI.Locales;
+using Microsoft.Extensions.Localization;
+
+public class LocalizedRequiredAttribute : RequiredAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        var _str = (IStringLocalizer)
+            validationContext.GetService(typeof(IStringLocalizer<Errors>))!;
+
+        string errorMessage = _str["requiredField", validationContext.DisplayName];
+
+        if (value == null || (value is string str && string.IsNullOrWhiteSpace(str)))
+        {
+            return new ValidationResult(errorMessage);
+        }
+
+        return ValidationResult.Success!;
+    }
+}

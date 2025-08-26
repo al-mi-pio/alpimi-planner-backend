819f261744f0b7621fd8b26cc89f4d263ef99ee0
piotr102k <piotrswiszcz2002@wp.pl>
2025-08-26 23:35:11 +0200

    Add ModifyDate to database

---
+++ Migrations/20250826211748_ScheduleModifyDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alpimi_planner_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScheduleModifyDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Schedule",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifyDate",
+                table: "Schedule");
+        }
+    }
+}

76e541f36fa4106caf8b3cf149938ccb4056c188
piotr102k <piotrswiszcz2002@wp.pl>
2025-10-17 23:53:11 +0200

    Add getting custom limiter settings from ENV

--- API/Settings/RateLimiterSettings.cs
+++ API/Settings/RateLimiterSettings.cs
@@ -2,8 +2,19 @@
 {
     public static class RateLimiterSettings
     {
-        public const int permitLimit = 30;
+        public const int aggressivePermitLimit = 10;
+        public static TimeSpan aggressiveTimeWindow = TimeSpan.FromSeconds(600);
 
-        public static TimeSpan timeWindow = TimeSpan.FromSeconds(5);
+        public const int strictPermitLimit = 10;
+        public static TimeSpan strictTimeWindow = TimeSpan.FromSeconds(10);
+
+        public const int regularPermitLimit = 20;
+        public static TimeSpan regularTimeWindow = TimeSpan.FromSeconds(5);
+
+        public const int moderatePermitLimit = 50;
+        public static TimeSpan moderateTimeWindow = TimeSpan.FromSeconds(5);
+
+        public const int loosePermitLimit = 100;
+        public static TimeSpan looseTimeWindow = TimeSpan.FromSeconds(5);
     }
 }
--- API/Utilities/Configuration.cs
+++ API/Utilities/Configuration.cs
@@ -22,18 +22,50 @@ namespace AlpimiAPI.Utilities
             "HASH_ALGORITHM"
         );
         private static readonly string? _keySize = Environment.GetEnvironmentVariable("KEY_SIZE");
+
         private static readonly string? _testConnectionString = Environment.GetEnvironmentVariable(
             "TEST_CONNECTION_STRING"
         );
-        private static readonly string? _permitLimit = Environment.GetEnvironmentVariable(
-            "PERMIT_LIMIT"
+
+        private static readonly string? _aggressivePermitLimit = Environment.GetEnvironmentVariable(
+            "AGGRESSIVE_PERMIT_LIMIT"
         );
-        private static readonly string? _timeWindow = Environment.GetEnvironmentVariable(
-            "TIME_WINDOW"
+        private static readonly string? _aggressiveTimeWindow = Environment.GetEnvironmentVariable(
+            "AGGRESSIVE_TIME_WINDOW"
         );
+
+        private static readonly string? _strictPermitLimit = Environment.GetEnvironmentVariable(
+            "STRICT_PERMIT_LIMIT"
+        );
+        private static readonly string? _strictTimeWindow = Environment.GetEnvironmentVariable(
+            "STRICT_TIME_WINDOW"
+        );
+
+        private static readonly string? _regularPermitLimit = Environment.GetEnvironmentVariable(
+            "REGULAR_PERMIT_LIMIT"
+        );
+        private static readonly string? _regularTimeWindow = Environment.GetEnvironmentVariable(
+            "REGULAR_TIME_WINDOW"
+        );
+
+        private static readonly string? _moderatePermitLimit = Environment.GetEnvironmentVariable(
+            "MODERATE_PERMIT_LIMIT"
+        );
+        private static readonly string? _moderateTimeWindow = Environment.GetEnvironmentVariable(
+            "MODERATE_TIME_WINDOW"
+        );
+
+        private static readonly string? _loosePermitLimit = Environment.GetEnvironmentVariable(
+            "LOOSE_PERMIT_LIMIT"
+        );
+        private static readonly string? _looseTimeWindow = Environment.GetEnvironmentVariable(
+            "LOOSE_TIME_WINDOW"
+        );
+
         private static readonly string? _historyLimit = Environment.GetEnvironmentVariable(
             "HISTORY_LIMIT"
         );
+
         public static int maxSchoolYearDuration { get; set; } = 24;
         public const int perPage = PaginationSettings.perPage;
         public const int page = PaginationSettings.page;
@@ -138,22 +170,94 @@ namespace AlpimiAPI.Utilities
             return CharacterSettings.AllowedCharacterTypesForScheduleName;
         }
 
-        public static int GetPermitLimit()
+        public static int GetAggressivePermitLimit()
         {
-            if (_permitLimit == null)
+            if (_aggressivePermitLimit == null)
             {
-                return RateLimiterSettings.permitLimit;
+                return RateLimiterSettings.aggressivePermitLimit;
             }
-            return Convert.ToInt32(_permitLimit);
+            return Convert.ToInt32(_aggressivePermitLimit);
         }
 
-        public static TimeSpan GetTimeWindow()
+        public static TimeSpan GetAggressiveTimeWindow()
         {
-            if (_timeWindow == null)
+            if (_aggressiveTimeWindow == null)
             {
-                return RateLimiterSettings.timeWindow;
+                return RateLimiterSettings.aggressiveTimeWindow;
             }
-            return TimeSpan.FromSeconds(Convert.ToInt32(_timeWindow));
+            return TimeSpan.FromSeconds(Convert.ToInt32(_aggressiveTimeWindow));
+        }
+
+        public static int GetStrictPermitLimit()
+        {
+            if (_strictPermitLimit == null)
+            {
+                return RateLimiterSettings.strictPermitLimit;
+            }
+            return Convert.ToInt32(_strictPermitLimit);
+        }
+
+        public static TimeSpan GetStrictTimeWindow()
+        {
+            if (_strictTimeWindow == null)
+            {
+                return RateLimiterSettings.strictTimeWindow;
+            }
+            return TimeSpan.FromSeconds(Convert.ToInt32(_strictTimeWindow));
+        }
+
+        public static int GetRegularPermitLimit()
+        {
+            if (_regularPermitLimit == null)
+            {
+                return RateLimiterSettings.regularPermitLimit;
+            }
+            return Convert.ToInt32(_regularPermitLimit);
+        }
+
+        public static TimeSpan GetRegularTimeWindow()
+        {
+            if (_regularTimeWindow == null)
+            {
+                return RateLimiterSettings.regularTimeWindow;
+            }
+            return TimeSpan.FromSeconds(Convert.ToInt32(_regularTimeWindow));
+        }
+
+        public static int GetModeratePermitLimit()
+        {
+            if (_moderatePermitLimit == null)
+            {
+                return RateLimiterSettings.moderatePermitLimit;
+            }
+            return Convert.ToInt32(_moderatePermitLimit);
+        }
+
+        public static TimeSpan GetModerateTimeWindow()
+        {
+            if (_moderateTimeWindow == null)
+            {
+                return RateLimiterSettings.moderateTimeWindow;
+            }
+            return TimeSpan.FromSeconds(Convert.ToInt32(_moderateTimeWindow));
+        }
+
+        public static int GetLoosePermitLimit()
+        {
+            if (_loosePermitLimit == null)
+            {
+                return RateLimiterSettings.loosePermitLimit;
+            }
+            return Convert.ToInt32(_loosePermitLimit);
+        }
+
+        public static TimeSpan GetLooseTimeWindow()
+        {
+            if (_looseTimeWindow == null)
+            {
+                return RateLimiterSettings.looseTimeWindow;
+            }
+            return TimeSpan.FromSeconds(Convert.ToInt32(_looseTimeWindow));
         }
     }
 }

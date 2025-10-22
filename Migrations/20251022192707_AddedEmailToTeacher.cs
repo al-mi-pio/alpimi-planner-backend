7c9c1917c55fa45b91d755aa14234e19b9c48dad
piotr102k <piotrswiszcz2002@wp.pl>
2025-10-22 22:00:21 +0200

    Add updated teacher to database

---
+++ Migrations/20251022192707_AddedEmailToTeacher.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace alpimi_planner_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEmailToTeacher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Teacher",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Teacher");
+        }
+    }
+}
